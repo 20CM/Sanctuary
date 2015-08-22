6b2fd1529bd35953e3d4e8c4b3dc996767fc3b56
whtsky <whtsky@gmail.com>
2015-08-22 20:37:24 +0800

    Bug fix

--- sanctuary/tag/api.py
+++ sanctuary/tag/api.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import SAFE_METHODS, IsAuthenticatedOrReadOnly, BasePermission
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 from .models import Tag
 from .serializers import TagSerializer
@@ -9,7 +9,7 @@ class IsAdminUserOrModerator(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
-        return  request.user and request.user.is_staff
+        return request.user and request.user.is_staff
 
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
