e9b560653ada7ddc5cdd8608f95508a3c2d96f18
whtsky <whtsky@gmail.com>
2015-08-23 23:00:31 +0800

    Add STATIC_ROOT

--- sanctuary/sanctuary/settings.py
+++ sanctuary/sanctuary/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MAX_MENTIONS_PER_REPLY = 5
 
