a59a8c80648d6c1ad2c600e336675b03d69e177e
whtsky <whtsky@gmail.com>
2015-08-23 18:33:50 +0800

    code improvement

--- sanctuary/topic/api.py
+++ sanctuary/topic/api.py
@@ -11,15 +11,8 @@ class CreateWithAuthorMixin(object):
     """
     Create a model instance.
     """
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer, author=request.user)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-    def perform_create(self, serializer, author):
-        serializer.save(author=author)
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
 class IsSuperAdminOrAuthor(BasePermission):
